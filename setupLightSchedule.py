706fd66fb865e17c4f21e59eca57ee08dae8282f
Adam Watkins <acwatkins@gmail.com>
2015-01-03 22:41:52 -0500

    Switched to a noon time schedule creation of evening and morning, rather than morning and evening of one day

--- setupLightSchedule.py
+++ setupLightSchedule.py
@@ -26,14 +26,6 @@ allLights = ["FamilyRoomTorch", "FamilyRoomLeft", "FamilyRoomRight", "MasterBedr
 familyRoom = ["FamilyRoomTorch", "FamilyRoomLeft", "FamilyRoomRight"]
 masterBedroom = ["MasterBedroomHis", "MasterBedroomHers"]
 
-def getUtcTimeString(hour, minute, second):
-	currentDateTime = datetime.datetime.now()
-	localCurrentDateTime = localtz.localize(currentDateTime)#, is_dst=None)
-	localDateTime = localCurrentDateTime
-	localDateTime = localDateTime.replace(hour = hour, minute = minute, second = second)
-	utcDateTime = localDateTime.astimezone(pytz.utc)
-	return utcDateTime.strftime("%Y-%m-%dT%H:%M:%S")
-
 def transitionRelaxToEnergize(beginHour, beginMinute, endHour, endMinute, lightNames):
 	endInMinutes = (endHour * 60) + endMinute
 	beginInMinutes = (beginHour * 60) + beginMinute
@@ -96,50 +88,63 @@ def bedTimeRoutine(beginHour, beginMinute, lightNames):
 	schedule.addGroupEventByOffsetToLast(30 * 60 * 10, lightNames, 'orangeLow', 15 * 60 * 10)
 	schedule.addGroupEventByOffsetToLast(30 * 60 * 10, lightNames, 'orangeLow', 0, lightOn = False)
 
-bridge = Bridge('huebridge', 'newdeveloper')
-# bridge.connect()
-#bridge.get_api()
+def setupSchedule(bedTimeHisHour, bedTimeHisMinute, bedTimeHersHour, bedTimeHersMinute, sleepDuration):
+	bridge = Bridge('huebridge', 'newdeveloper')
+	# bridge.connect()
+	#bridge.get_api()
 
-# lightsByName = bridge.get_light_objects('name')
-# for light in lightsByName:
-# 	print(str(light))
+	# lightsByName = bridge.get_light_objects('name')
+	# for light in lightsByName:
+	# 	print(str(light))
 
-schedules = bridge.get_schedule()
-logging.info("Removing old schedules")
-for i in schedules:
-	bridge.delete_schedule(i)
+	schedules = bridge.get_schedule()
+	logging.info("Removing old schedules")
+	for i in schedules:
+		bridge.delete_schedule(i)
 
-weekday = datetime.datetime.now().weekday()
-if (weekday == 1 or weekday == 5 or weekday == 6):
-	schedule.addGroupEvent(5, 45, 0, masterBedroom, 'orangeLow', 0, lightOn = True)
-	schedule.addGroupEvent(6, 15, 0, masterBedroom, 'orangeLow', 0, lightOn = False)
+	logging.info(sunsetLightsOnTime.strftime("%H:%M:%S turn on lights"))
+	schedule.addGroupEvent(sunsetLightsOnTime.hour, sunsetLightsOnTime.minute, sunriseLightsOffTime.second, allLights, 'energize', 30 * 60 * 10, lightOn = True)
 
-	morningRoutine(5, 45, familyRoom)
-	morningRoutine(6, 45, masterBedroom)
-else:
-	morningRoutine(5, 45, allLights)
+	logging.info("sunset is at " + str(sun["sunset"]))
+	transitionEnergizeToRelax(sun["sunset"].hour, sun["sunset"].minute, 21, 0, allLights)
 
-logging.info("sunrise is at " + str(sun["sunrise"]))
-logging.info(sunriseLightsOffTime.strftime("%H:%M:%S turn off lights"))
-schedule.addGroupEvent(sunriseLightsOffTime.hour, sunriseLightsOffTime.minute, sunriseLightsOffTime.second, allLights, 'energize', 30 * 60 * 10, lightOn = False)
+	bedTimeHis = schedule.getLocalDateTime(bedTimeHisHour, bedTimeHisMinute, 0)
+	bedTimeHers = schedule.getLocalDateTime(bedTimeHersHour, bedTimeHersMinute, 0)
+	bedTimeStartTimeMasterBedroom = bedTimeHers - datetime.timedelta(minutes = 30)
+	bedTimeStartTimeFamilyRoom = bedTimeHis - datetime.timedelta(minutes = 30)
+	if (bedTimeHisHour != bedTimeHersHour or bedTimeHisMinute != bedTimeHersMinute):
+		bedTimeRoutine(bedTimeStartTimeMasterBedroom.hour, bedTimeStartTimeMasterBedroom.minute, masterBedroom)
+		bedTimeRoutine(bedTimeStartTimeFamilyRoom.hour, bedTimeStartTimeFamilyRoom.minute, familyRoom)
 
-logging.info(sunsetLightsOnTime.strftime("%H:%M:%S turn on lights"))
-schedule.addGroupEvent(sunsetLightsOnTime.hour, sunsetLightsOnTime.minute, sunriseLightsOffTime.second, allLights, 'energize', 30 * 60 * 10, lightOn = True)
+		turnOffMasterBedroomLightTime = bedTimeHis + datetime.timedelta(minutes = 30)
+		schedule.addEvent(bedTimeHis.hour, bedTimeHisMinute, 0, 'MasterBedroomHis', 'orangeLow', 0, lightOn = True)
+		schedule.addEvent(turnOffMasterBedroomLightTime.hour, turnOffMasterBedroomLightTime.minute, 0, 'MasterBedroomHis', 'orangeLow', 0, lightOn = False)
+	else:
+		bedTimeRoutine(bedTimeStartTimeFamilyRoom.hour, bedTimeStartTimeFamilyRoom.minute, allLights)
 
-logging.info("sunset is at " + str(sun["sunset"]))
-transitionEnergizeToRelax(sun["sunset"].hour, sun["sunset"].minute, 21, 0, allLights)
+	wakeUpTimeHis = bedTimeHis + datetime.timedelta(hours = sleepDuration)
+	wakeUpTimeHers = schedule.getLocalDateTime(6, 0, 0)
+	if (wakeUpTimeHis != wakeUpTimeHers):
+		lightsOnHers = wakeUpTimeHers - datetime.timedelta(minutes = 15)
+		lightsOffHers = wakeUpTimeHers + datetime.timedelta(minutes = 15)
+		schedule.addEvent(lightsOnHers.hour, lightsOnHers.minute, 0, 'MasterBedroomHers', 'orangeLow', 0, lightOn = True)
+		schedule.addGroupEvent(lightsOffHers.hour, lightsOffHers.minute, 0, 'MasterBedroomHers', 'orangeLow', 0, lightOn = False)
 
-if (weekday == 4 or weekday == 5):
-	bedTimeRoutine(21, 30, masterBedroom)
-	bedTimeRoutine(22, 30, familyRoom)
-	schedule.addEvent(22, 55, 0, 'MasterBedroomHis', 'orangeLow', 0, lightOn = True)
-	schedule.addEvent(23, 30, 0, 'MasterBedroomHis', 'orangeLow', 0, lightOn = False)
-else:
-	bedTimeRoutine(21, 30, allLights)
+		morningRoutine(lightsOnHers.hour, lightsOnHers.minute, familyRoom)
+		lightsOnHis = wakeUpTimeHis - datetime.timedelta(minutes = 15)
+		morningRoutine(lightsOnHis.hour, lightsOnHis.minute, masterBedroom)
+	else:
+		lightsOnTime = wakeUpTimeHers - datetime.timedelta(minutes = 15)
+		morningRoutine(lightsOnTime.hour, lightsOnTime.minute, allLights)
 
-if (weekday == 0):
-	schedule.addGroupEvent(23, 0, 0, familyRoom, 'relax', 60 * 10, lightOn = True)
-	schedule.addEvent(23, 0, 0, 'MasterBedroomHis', 'orangeLow', 0, lightOn = True)
+	logging.info("sunrise is at " + str(sun["sunrise"]))
+	logging.info(sunriseLightsOffTime.strftime("%H:%M:%S turn off lights"))
+	schedule.addGroupEvent(sunriseLightsOffTime.hour, sunriseLightsOffTime.minute, sunriseLightsOffTime.second, allLights, 'energize', 30 * 60 * 10, lightOn = False)
 
-	schedule.addGroupEvent(23, 30, 0, familyRoom, 'orangeLow', 30 * 60 * 10, lightOn = False)
-	schedule.addGroupEvent(23, 59, 0, 'MasterBedroomHis', 'orangeLow', 0, lightOn = False)
+
+if __name__ == '__main__':
+	weekday = datetime.datetime.now().weekday()
+	if (weekday == 4 or weekday == 5):
+		setupSchedule(23, 0, 22, 0, 8)
+	else:
+		setupSchedule(22, 0, 22, 0, 8)
